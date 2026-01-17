56c879626f67b78c12fc9941d1d71076d9c9adc9
Manas Upadhyay <manasupadhyay1318@gmail.com>
2026-01-17 23:29:59 +0530

    disable onkeo in dev

--- components/oneko/OnekoCat.tsx
+++ components/oneko/OnekoCat.tsx
@@ -1,5 +1,8 @@
 import Script from 'next/script';
 
 export default function OnekoCat() {
+    if(process.env.NODE_ENV === 'development') {
+        return null;
+    }
   return <Script src="./oneko/oneko.js" data-cat="./oneko/oneko.gif" />;
 }
